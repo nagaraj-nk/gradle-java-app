4371e2c4b31ab50ea75a633e9eeb8a5ef4334ead
Nagaraj N <nnagarajece31@gmail.com>
2022-08-27 09:24:25 +0530

    initial commit

---
+++ src/main/java/com/srjons/gradleproject/GradleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.srjons.gradleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srjons/gradleproject/GradleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srjons.gradleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
